9f773fbc58d88a8be2ca5d6bb31f152003f90034
parth-tiwari10 <71581858+parth-tiwari10@users.noreply.github.com>
2020-12-04 13:48:52 +0530

    code uploaded

---
+++ translation.py
@@ -0,0 +1,47 @@
+class Translation(object):
+    START_TEXT = """Hello,
+This is a Telegram File convert Bot!
+You can convert any Telegram Document, using this bot! With custom thumbnail
+				join
+				@ben10_hindispot
+                @cartoon_in
+                @movies_hindispot
+                @hindispot_family
+     
+    """
+    ABS_TEXT = " Please don't be selfish."
+    BANNED_USER_TEXT = "you are banned"
+    UPGRADE_TEXT = "No upgrade plan 🥰"
+    DOWNLOAD_START = "trying to download"
+    UPLOAD_START = "trying to upload"
+    RCHD_BOT_API_LIMIT = "size greater than maximum allowed size (50MB). Neverthless, trying to upload."
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "http://t.me/filetovodbot"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds. \nPlease rate me if you find me useful. https://t.me/tlgrmcbot?start=anydl_bot-bot \nUploaded in {} seconds."
+    NOT_AUTH_USER_TEXT = "Please /upgrade your subscription."
+    SAVED_CUSTOM_THUMB_NAIL = "Custom video / file thumbnail saved. This image will be used in next 24 hr"
+    DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully."
+    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "✅ Media cleared succesfully."
+    SAVED_RECVD_DOC_FILE = "Document Downloaded Successfully."
+    CUSTOM_CAPTION_UL_FILE = ""
+    NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom ThumbNail found."
+    CURENT_PLAN_DETAILS = """Current plan details
+--------
+Telegram ID: <code>{}</code>
+Plan name: <a href='https://t.me/hindispot_family'>{}</a>
+Expires on: {}"""
+    HELP_USER = """I can do:
+👉 <a href="http://t.me/filetovodbot">Convert To Streamable video any telegram file</a>
+--------
+
+🔥 How to deploy : <a href="t.me/kingt10">Click here</a>
+
+    © Group   : @hindispot_family
+    © Channel : @ben10_hindispot
+                @cartoon_in
+                @movies_hindispot
+    © credits : @kingt10
+Send /me to know current plan details
+
+"""
+
+    REPLY_TO_DOC_FOR_C2V = "Reply to a Telegram media to convert"
